d4e57b5419515ddd37eb1ab5ff2ac93102ce7594
dkStephanos <stephanos@goldmail.etsu.edu>
2020-05-04 18:53:22 -0400

    Adding CLIInterface

---
+++ LastParser/CLIInterface.py
@@ -0,0 +1,17 @@
+class CLIInterface(object):
+    #Logs status and code to screen
+    @staticmethod
+    def log(status, code):
+        print(status + "Exiting with code 2\n" if code == 2 else "Continuing with code {0}\n".format(code))
+    
+    #Parses args array from user, returning dict of filepaths (using std where appropriate)
+    #.\Data\(Windows format) 2016 10 29 valid data.txt is default input file
+    def parseArgs(args):
+        settings = {'input_file': ".\Data\(Windows format) 2016 10 29 valid data.txt",
+                    'markup_file': "parsed_output.txt", 'error_file': "errors.txt",
+                    'suspense_file': "errors.txt", 'language': 'JSON'}
+        #parse args
+
+
+        return settings
+
