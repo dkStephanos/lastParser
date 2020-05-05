abab7bd12e7b3f75c85be491f27711859a72bf96
dkStephanos <stephanos@goldmail.etsu.edu>
2020-05-05 13:46:23 -0400

    Added skeleton for RegExParser

---
+++ LastParser/RegExParser.py
@@ -0,0 +1,14 @@
+class RegExParser(object):
+    """Checks record types and parses them using masks"""
+
+    def checkRecord(record):
+        
+        #return record type
+        pass
+
+    def parseRecord(record, record_type):
+
+        #return [parsed_record, flags, flagged_rows]
+        pass
+
+
