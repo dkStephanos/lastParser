37fea29cf26a84649422dc8552a972f9d7773911
Koi Stephanos <koi.stephanos@rbdom.rbinv.io>
2020-05-05 16:43:51 -0400

    Now parsing/creating record for first type

--- LastParser/LastParser.py
+++ LastParser/LastParser.py
@@ -2,10 +2,13 @@ from FileHandler import FileHandler
 from DataUtil import DataUtil
 from Records.SysCrashRecord import SysCrashRecord
 from RegExParser import RegExParser
+import json
 
 if __name__ == "__main__":
     rawData = FileHandler.getContentsOfFile('.\Data\(Windows format) 2016 10 29 valid data.txt')
 
     records = DataUtil.getRecordsFromRawData(rawData)
 
-    RegExParser.checkRecord(records[0])
+    record = RegExParser.checkAndParseRecords([records[0]])
+
+    print(json.dumps(record[0].record))
--- LastParser/RegExParser.py
+++ LastParser/RegExParser.py
@@ -1,4 +1,5 @@
 import re
+from Records.LgnKnwnIncomplete import LgnKnwnIncompleteRecord
 
 class RegExParser(object):
     """Checks record types and parses them using masks"""
@@ -8,8 +9,9 @@ class RegExParser(object):
                    'SysRunLvlChange': "runlevel \(to lvl [0-9]\)\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2} - (Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*\([0-9]{0,2}\+?[0-9]{0,2}:[0-9]{2}\)\s*2.6.32-([0-9]{0,3}.){3}el[0-9].i[0-9]{1,3}",
                    'LgnKnwnCrash': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2} - down\s*\([0-9]{0,2}\+?[0-9]{0,2}:[0-9]{2}\)\s*(((([0-9]|[a-zA-Z])+)+(.|-)?)+([0-9]|[a-zA-Z])*)",
                    'LgnKnwnComplete': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2} - (Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*\([0-9]{0,2}\+?[0-9]{0,2}:[0-9]{2}\)\s*(((([0-9]|[a-zA-Z])+)+(.|-)?)+([0-9]|[a-zA-Z])*)",
-                   'LgnKnwnIncomplete': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2} - (Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*\([0-9]{0,2}\+?[0-9]{0,2}:[0-9]{2}\)$",
-                   'LgnUnknwnComplete': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*still logged in\s*(((([0-9]|[a-zA-Z])+)+(.|-)?)+([0-9]|[a-zA-Z])*)"}
+                   'LgnUnknwnComplete': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2} - (Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*\([0-9]{0,2}\+?[0-9]{0,2}:[0-9]{2}\)$",
+                   'LgnKnwnIncomplete': "[a-z]{0,32}\s*(pts\/)[0-9]+\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} 2(0|1)[0-9]{2}\s*still logged in\s*(((([0-9]|[a-zA-Z])+)+(.|-)?)+([0-9]|[a-zA-Z])*)"}
+
 
     #Checks record against mask, returning the corresponding record type if a match, else -1
     @staticmethod
@@ -23,17 +25,41 @@ class RegExParser(object):
     
     @staticmethod
     def parseRecord(record, record_type):
-
-        #return [parsed_record, flags, flagged_rows]
-        pass
+        return RegExParser.record_parsers[record_type](record)
 
     @staticmethod
     def checkAndParseRecords(records):
         parsed_records = []
 
         for record in records:
-            type = checkRecord(record)
-            parsed_record = parseRecord(record, type)
+            type = RegExParser.checkRecord(record)
+            parsed_record = RegExParser.parseRecord(record, type)
             parsed_records.append(parsed_record)
 
         return parsed_records
+
+    def parseLgnKnwnIncomplete(record):
+        parsedRecord = {'incomplete': {'user': '', 'pts-terminal': '', 'start-session': {'date': {'year': '', 'month': '', 'day': '', 'weekday': ''}, 'time': {'hr': '', 'mn': '', 'sec': ''}, 'remote-terminal': ''}}}
+        record_arr = record.split()
+
+        parsedRecord['incomplete']['user'] = record_arr[0]
+        parsedRecord['incomplete']['pts-terminal'] = record_arr[1]
+        parsedRecord['incomplete']['start-session']['date']['year'] = record_arr[6]
+        parsedRecord['incomplete']['start-session']['date']['month'] = record_arr[3]
+        parsedRecord['incomplete']['start-session']['date']['day'] = record_arr[4]
+        parsedRecord['incomplete']['start-session']['date']['weekday'] = record_arr[2]
+        parsedRecord['incomplete']['start-session']['time']['hr'] = record_arr[5].split(':')[0]
+        parsedRecord['incomplete']['start-session']['time']['mn'] = record_arr[5].split(':')[1]
+        parsedRecord['incomplete']['start-session']['time']['sec'] = record_arr[5].split(':')[2]
+        parsedRecord['incomplete']['start-session']['remote-terminal'] = record_arr[10]
+
+
+        return LgnKnwnIncompleteRecord(parsedRecord)
+
+    
+    
+    
+    
+    
+    #Dispatch table for record specific parsers
+    record_parsers = {'LgnKnwnIncomplete': parseLgnKnwnIncomplete}
